5d38c172104294d961eed726c3754a86e7bcb4e2
Sabina <Sabinagli45@gmail.com>
2021-03-31 10:14:07 +0300

    Init my project

---
+++ DailyMood/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DailyMood
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void MainButtonClick(object sender, RoutedEventArgs e)
+        {
+            int index = int.Parse(((Button)e.Source).Uid);
+            Binding binding = new Binding();
+
+            switch (index)
+            {
+                case 0:
+                    GridSingUpPage.Visibility = Visibility.Visible;
+                    GridSingInPage.Visibility = Visibility.Collapsed;
+                    GridSingPage.Visibility = Visibility.Collapsed;
+                    GridHomePage.Visibility = Visibility.Collapsed;
+
+
+                    break;
+                case 1:
+                    GridSingUpPage.Visibility = Visibility.Collapsed;
+                    GridSingPage.Visibility = Visibility.Collapsed;
+                    GridSingInPage.Visibility = Visibility.Visible;
+                    GridHomePage.Visibility = Visibility.Collapsed;
+                    break;
+                case 2:
+                    GridSingUpPage.Visibility = Visibility.Collapsed;
+                    GridSingPage.Visibility = Visibility.Visible;
+                    GridSingInPage.Visibility = Visibility.Collapsed;
+                    GridHomePage.Visibility = Visibility.Collapsed;
+                    break;
+                case 3:
+                    GridSingUpPage.Visibility = Visibility.Collapsed;
+                    GridSingPage.Visibility = Visibility.Collapsed;
+                    GridSingInPage.Visibility = Visibility.Collapsed;
+                    GridHomePage.Visibility = Visibility.Visible;
+                    break;
+
+
+
+            }
+
+        }
+    }
+    
+}
