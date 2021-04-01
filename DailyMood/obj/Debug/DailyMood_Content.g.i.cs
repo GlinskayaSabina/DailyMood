4472753ed47e0fc53a76bcd560d640c661c3362c
Sabina <Sabinagli45@gmail.com>
2021-04-01 17:14:00 +0300

    Add phto adder

--- DailyMood/obj/Debug/DailyMood_Content.g.i.cs
+++ DailyMood/obj/Debug/DailyMood_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("assets/camera.svg")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("icon.ico")]
 
 
